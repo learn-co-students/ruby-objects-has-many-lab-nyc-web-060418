8d4881740437e2f96c2a3a673bb063573f25eb47
DevinPierce <devinmpierce@gmail.com>
2018-06-07 17:55:30 -0400

    done

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+class Artist
+
+  attr_accessor :name, :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    song.artist = self
+    @songs << song
+  end
+
+  def add_song(song)
+    self.songs << song
+    song.artist = self
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
+    self.songs << song
+  end
+
+  def self.song_count
+    Song.all.count
+  end
+
+end
