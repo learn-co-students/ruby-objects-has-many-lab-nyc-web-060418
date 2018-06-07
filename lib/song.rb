fa596d2549442607195069955e8c48760d830890
KoichiSakamoto <isaac.fiore@mail.mcgill.ca>
2018-06-07 09:58:01 -0400

    implemented

---
+++ lib/song.rb
@@ -0,0 +1,22 @@
+require 'pry'
+
+class Song
+  attr_accessor :name, :artist
+  @@all = []
+
+  def initialize(name, artist = nil)
+    @name = name
+    @artist = artist
+    @@all << self
+  end
+
+  def artist_name
+    if @artist == nil
+      nil
+    else
+      @artist.name
+    end
+  end
+
+
+end
