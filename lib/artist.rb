079b6f45e79f6ce4c69144b5fe39ce1c4707c2d2
Ms-Alex <ms.hernandeza1@gmail.com>
2018-06-09 14:10:47 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,38 @@
+
+
+class Artist
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all << self
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+    song.artist = self
+    @songs << song
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
+    @songs << song
+  end
+
+  def self.song_count
+    count = 0
+    @@all.each do |artist|
+      count += artist.songs.count
+    end
+    count
+  end
+
+end
