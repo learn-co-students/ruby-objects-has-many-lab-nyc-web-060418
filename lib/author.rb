efa9f5ef3c5e3ce2009ff4f3003b53640187babc
newmanbradm <newmanbradm@gmail.com>
2018-06-09 16:39:47 -0400

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,30 @@
+class Author
+
+  attr_accessor :name, :posts
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def add_post(post)
+    post.author << self
+    self.posts << post
+  end
+
+  def add_post(post)
+    post.author = self
+    self.posts << post
+  end
+
+  def add_post_by_title(post_title)
+    post = Post.new(post_title)
+    self.add_post(post)
+    post.title
+  end
+
+  def self.post_count
+    Post.all.length
+  end
+
+end
